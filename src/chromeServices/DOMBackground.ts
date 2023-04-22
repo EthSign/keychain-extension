6306eb81f84034b58c8d3e3b6b6c3ae3663f4da0
Jordan Bettencourt <jbettenc@usc.edu>
2023-04-21 21:26:26 -0700

    Bump snap version to 0.2.0

--- src/chromeServices/DOMBackground.ts
+++ src/chromeServices/DOMBackground.ts
@@ -3,7 +3,7 @@ import _ from "lodash";
 import { createExternalExtensionProvider } from "@metamask/providers";
 
 const DEFAULT_SNAP_ID = "npm:w3ptestsnap"; // "local:http://localhost:8081";
-const SNAP_VERSION = "0.1.9";
+const SNAP_VERSION = "0.2.0";
 let activeTabId: number;
 
 /// Begin snap functions
